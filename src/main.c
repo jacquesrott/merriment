f025a56f6ab8fd8a2dabcd4795212f8fabe02099
Jacques Rott <jacques.rott@gmail.com>
2014-12-25 23:21:55 +0100

    Make application with opengl context and fixed timeline

--- src/main.c
+++ src/main.c
@@ -1,18 +1,113 @@
 #include <SDL2/SDL.h>
+#include <OpenGL/gl3.h>
+
 #include <stdio.h>
 
 
-int main() {
-    int width = 1440;
-    int height = 900;
+#define DT 10
 
-    SDL_Window* window = NULL;
-    if (-1 == SDL_Init(SDL_INIT_EVERYTHING)) {
-        printf("Failed to initialize SDL error=%s\n", SDL_GetError());
-        exit(1);
+
+void print_error(const char* message) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s SDL error=%s\n", message);
+    exit(1);
+} 
+
+
+typedef struct {
+    int width;
+    int height;
+    int run;
+    SDL_Event event;
+    SDL_Window* window;
+    SDL_GLContext gl;
+    unsigned int last_time;
+} Game;
+
+
+Game* game_create(int width, int height) {
+    Game* game = malloc(sizeof(*game));
+
+    game->width = width;
+    game->height = height;
+    game->run = 0;
+
+    game->window = SDL_CreateWindow(
+            "Dwarves",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            width, height,
+            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+    if(game->window == NULL) {
+        print_error("Failed to create window");
     }
 
-    atexit(SDL_Quit);
+    game->gl = SDL_GL_CreateContext(game->window);
 
-    return 0;
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    game->last_time = SDL_GetTicks();
+
+    return game;
 }
+
+
+Game* dwarves;
+
+
+void quit_dwarves() {
+    if(dwarves->window) {
+        SDL_GL_DeleteContext(dwarves->gl);
+        SDL_DestroyWindow(dwarves->window);
+    }
+    SDL_Quit();
+    free(dwarves);
+    printf("Quitting Dwarves.\n");
+}
+
+
+int main() {
+    int width = 1440, height = 900;
+    unsigned int frame_time, current_time, accumulator = 0;
+
+    if(SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+        print_error("Failed to initialize");
+    }
+    atexit(quit_dwarves);
+
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
+
+    dwarves = game_create(width, height);
+
+    while(dwarves->run == 0) {
+        current_time = SDL_GetTicks();
+        frame_time = current_time - dwarves->last_time;
+        dwarves->last_time = current_time;
+
+        accumulator += frame_time;
+        while(SDL_PollEvent(&dwarves->event)) {
+            switch(dwarves->event.type) {
+                case SDL_QUIT:
+                    dwarves->run = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        while(accumulator >= DT) {
+            accumulator -= DT;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(dwarves->window);
+    }
+
+    return EXIT_SUCCESS;
+}
+

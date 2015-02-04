c510f8a7c281db2331c5c7c948e6926d1ea07523
Jacques Rott <jacques.rott@gmail.com>
2015-02-04 09:12:55 +0100

    Fix planet generation

--- src/planet.c
+++ src/planet.c
@@ -39,7 +39,7 @@ void planet_generate(Planet* planet) {
 
     float radius = rand_rangef(DW_MIN_PLANET_RADIUS, DW_MAX_PLANET_RADIUS);
 
-    for(angle = 360.0; angle > 0.0; angle -= rand_rangef(1.0, 2.0)) {
+    for(angle = 360.0; angle > 0.0; angle -= 2.0) {
         radius = rand_rangef(DW_MIN_PLANET_RADIUS, DW_MAX_PLANET_RADIUS);
 
         float radian_angle = deg_to_rad(angle);
@@ -47,20 +47,19 @@ void planet_generate(Planet* planet) {
         ++counter;
     }
 
-    counter = round_down(counter, 3);
     planet->curve = curve_multiquadratic(points, counter);
 
-    int indices_size = planet->curve->points_length * 3;
+    int vertice_size = planet->curve->points_length + 1;
+    int indices_size = vertice_size * 3;
     unsigned int indices[indices_size];
     int center_index = planet->curve->points_length;
     int i;
     for(i = 0; i < indices_size; i += 3) {
-        indices[i] = center_index;
-        indices[i + 1] = i;
         indices[i + 2] = (i == indices_size - 1) ? 0 : i + 1;
+        indices[i + 1] = i;
+        indices[i] = center_index;
     }
 
-    int vertice_size = planet->curve->points_length + 1;
     vec2 vertices[vertice_size];
     vec2 center = {0, 0};
 
@@ -69,5 +68,5 @@ void planet_generate(Planet* planet) {
         planet->curve->points, sizeof(vec2) * planet->curve->points_length,
         &center, sizeof(vec2));
 
-    planet->mesh = mesh_create(planet->curve->points, planet->curve->points_length + 1, indices, indices_size);
+    planet->mesh = mesh_create(vertices, vertice_size, indices, indices_size);
 }

dbc14accdcdd571622f210f263c5c8f4ed322107
Jacques Rott <jacques.rott@gmail.com>
2015-03-09 22:39:20 +0100

    Fix sprite component pool structure

--- src/components/sprite.c
+++ src/components/sprite.c
@@ -1,12 +1,48 @@
+#include <SDL2/SDL.h>
 #include <stdlib.h>
 
 #include "sprite.h"
 
 
+static void pool_init(SpritePool* pool, unsigned int capacity) {
+    pool->available = &pool->items[0];
+    capacity = capacity - 1;
+    int i;
+    for(i = 0 ; i < capacity ; ++i) {
+        pool->items[i].pool.next = &pool->items[i + 1];
+    }
+    pool->items[capacity].pool.next = NULL;
+}
+
+
+void spritepool_destroy(SpritePool* pool) {
+    int i;
+    for(i = 0 ; i < pool->count ; ++i) {
+        SpriteComponent* item = &pool->items[i];
+        SpritePool* pool = item->pool.container;
+        pool->free_item(item);
+    }
+}
+
+
+static SpriteComponent* pool_pop_available(SpritePool* pool) {
+    SpriteComponent* item = pool->available;
+    pool->available = item->pool.next;
+    return item;
+}
+
+
+static void pool_set_available(SpritePool* pool, SpriteComponent* item) {
+    SpriteComponent* available = item;
+    item->pool.next = pool->available;
+    pool->available = available;
+}
+
+
 SpritePool* spritepool_create() {
     SpritePool* pool = malloc(sizeof(*pool));
-    pool_init(pool, MAX_SPRITES);
-    pool->free = spritepool_free;
+    pool_init(pool, MAX_RENDERERS);
+    pool->free_item = spritecomponent_free_pool;
     return pool;
 }
 
@@ -14,7 +50,7 @@ SpritePool* spritepool_create() {
 void* spritepool_add(SpritePool* pool, Entity* entity, Sprite* sprite, GLuint program) {
     SpriteComponent* item = pool_pop_available(pool);
     if(item == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SpritePool stack overflow.\n", name);
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SpritePool stack overflow.\n");
         return NULL;
     }
 
@@ -25,9 +61,9 @@ void* spritepool_add(SpritePool* pool, Entity* entity, Sprite* sprite, GLuint pr
     return item;
 }
 
-void spritepool_free(SpritePool* pool, SpriteComponent* item) {
+void spritecomponent_free_pool(SpriteComponent* item) {
     spritecomponent_destroy(item);
-    pool_set_available(pool, item);
+    pool_set_available(item->pool.container, item);
 }
 
 
--- src/components/sprite.h
+++ src/components/sprite.h
@@ -9,6 +9,7 @@
 
 
 typedef struct {
+    PoolItem pool;
     Entity* entity;
     Sprite* sprite;
     GLuint program;
@@ -19,13 +20,15 @@ typedef struct {
     SpriteComponent items[MAX_RENDERERS];
     SpriteComponent* available;
     unsigned int count;
-    void (*free_item)(void* item);
+    void (*free_item)(SpriteComponent* item);
 } SpritePool;
 
 
 SpritePool* spritepool_create();
 void* spritepool_add(SpritePool* pool, Entity* entity, Sprite* sprite, GLuint program);
-void spritepool_free(SpritePool* pool, SpriteComponent* item);
+void spritepool_destroy(SpritePool* pool);
+
+void spritecomponent_free_pool(SpriteComponent* item);
 void spritecomponent_destroy(SpriteComponent* component);
 
 

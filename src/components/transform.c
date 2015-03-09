08b70e87c3a13b085430e8dfc69961e21691956d
Jacques Rott <jacques.rott@gmail.com>
2015-03-09 09:13:01 +0100

    Convert transform component to be used in a pool

--- src/components/transform.c
+++ src/components/transform.c
@@ -3,6 +3,14 @@
 #include "transform.h"
 
 
+TransformPool* transformpool_create() {
+    TransformPool* pool = malloc(sizeof(*pool));
+    pool_init(pool, MAX_SPRITES);
+    pool->free = transformpool_free;
+    return pool;
+}
+
+
 static void transformcomponent_refresh(TransformCompoment* component) {
     mat4 rotation = m4_rotatez(component->angle),
          translate = m4_translate2(&component->position);
@@ -10,12 +18,25 @@ static void transformcomponent_refresh(TransformCompoment* component) {
 }
 
 
-TransformCompoment* transformcomponent_create(vec2 position, float angle) { 
-    TransformCompoment* component = malloc(sizeof(*component));
-    component->position = position;
-    component->angle = angle;
-    transformcomponent_refresh(component);
-    return component;
+void* transformpool_add(TransformPool* pool, Entity* entity, vec2 position, float angle) {
+    TransformComponent* item = pool_pop_available(pool);
+    if(item == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "TransformPool stack overflow.\n", name);
+        return NULL;
+    }
+
+    item->entity = entity;
+    item->position = position;
+    item->angle = angle;
+    transformcomponent_refresh(item);
+
+    return item;
+}
+
+
+void transformpool_free(TransformPool* pool, TransformComponent* item) {
+    transformcomponent_destroy(item);
+    pool_set_available(pool, item);
 }
 
 
--- src/components/transform.h
+++ src/components/transform.h
@@ -1,17 +1,31 @@
 #ifndef DW_TRANSFORM_COMPONENT_H
 #define DW_TRANSFORM_COMPONENT_H
 
-typedef struct TransformCompoment TransformCompoment;
-
+#include "../config.h"
+#include "../pool.h"
 #include "../almath.h"
 #include "../entity.h"
 
-struct TransformCompoment {
+
+typedef struct {
+    Entity* entity;
     vec2 position;
     float angle;
     mat4 transform;
-};
+} TransformCompoment;
 
+
+typedef struct {
+    TransformComponent items[MAX_TRANSFORMS];
+    TransformComponent* available;
+    unsigned int count;
+    void (*free_item)(void* item);
+} TransformPool;
+
+
+TransformPool* transformpool_create();
+void* transformpool_add(TransformPool* pool, Entity* entity, vec2 position, float angle);
+void transformpool_free(TransformPool* pool, TransformComponent* item);
 
 TransformCompoment* transformcomponent_create(vec2 position, float angle);
 void transformcomponent_destroy(TransformCompoment* component);

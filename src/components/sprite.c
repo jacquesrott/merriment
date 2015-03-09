e407e7f5cd6df5987c4d7d2f1c3b8ea9788d5861
Jacques Rott <jacques.rott@gmail.com>
2015-03-09 09:12:47 +0100

    Convert sprite component to be used in a pool

--- src/components/sprite.c
+++ src/components/sprite.c
@@ -3,16 +3,35 @@
 #include "sprite.h"
 
 
-SpriteComponent* spritecomponent_create(Sprite* sprite, GLuint program) {
-    SpriteComponent* component = malloc(sizeof(*component));
-    component->sprite = sprite;
-    component->program = program;
-    return component;
+SpritePool* spritepool_create() {
+    SpritePool* pool = malloc(sizeof(*pool));
+    pool_init(pool, MAX_SPRITES);
+    pool->free = spritepool_free;
+    return pool;
+}
+
+
+void* spritepool_add(SpritePool* pool, Entity* entity, Sprite* sprite, GLuint program) {
+    SpriteComponent* item = pool_pop_available(pool);
+    if(item == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SpritePool stack overflow.\n", name);
+        return NULL;
+    }
+
+    item->entity = entity;
+    item->sprite = sprite;
+    item->program = program;
+
+    return item;
+}
+
+void spritepool_free(SpritePool* pool, SpriteComponent* item) {
+    spritecomponent_destroy(item);
+    pool_set_available(pool, item);
 }
 
 
 void spritecomponent_destroy(SpriteComponent* component) {
     sprite_destroy(component->sprite);
     program_destroy(component->program);
-    free(component);
 }
--- src/components/sprite.h
+++ src/components/sprite.h
@@ -1,21 +1,31 @@
 #ifndef DW_SPRITE_COMPONENT_H
 #define DW_SPRITE_COMPONENT_H
 
-typedef struct SpriteComponent SpriteComponent;
 
+#include "../config.h"
 #include "../sprite.h"
 #include "../shader.h"
 #include "../entity.h"
 
 
-struct SpriteComponent {
+typedef struct {
     Entity* entity;
     Sprite* sprite;
     GLuint program;
-};
+} SpriteComponent;
 
 
-SpriteComponent* spritecomponent_create(Sprite* sprite, GLuint program);
+typedef struct {
+    SpriteComponent items[MAX_RENDERERS];
+    SpriteComponent* available;
+    unsigned int count;
+    void (*free_item)(void* item);
+} SpritePool;
+
+
+SpritePool* spritepool_create();
+void* spritepool_add(SpritePool* pool, Entity* entity, Sprite* sprite, GLuint program);
+void spritepool_free(SpritePool* pool, SpriteComponent* item);
 void spritecomponent_destroy(SpriteComponent* component);
 
 

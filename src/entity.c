c7656d83dd79e80d45d482c4011705512af66682
Jacques Rott <jacques.rott@gmail.com>
2015-03-09 22:38:41 +0100

    Fix and adapt entity with Component types

--- src/entity.c
+++ src/entity.c
@@ -1,13 +1,53 @@
+#include <SDL2/SDL.h>
 #include <stdlib.h>
 
 #include "entity.h"
 #include "pool.h"
 
+#include "components/physic.h"
+#include "components/transform.h"
+#include "components/script.h"
+
+
+static void pool_init(EntityPool* pool, unsigned int capacity) {
+    pool->available = &pool->items[0];
+    capacity = capacity - 1;
+    int i;
+    for(i = 0 ; i < capacity ; ++i) {
+        pool->items[i].pool.next = &pool->items[i + 1];
+    }
+    pool->items[capacity].pool.next = NULL;
+}
+
+
+void entitypool_destroy(EntityPool* pool) {
+    int i;
+    for(i = 0 ; i < pool->count ; ++i) {
+        Entity* item = &pool->items[i];
+        EntityPool* pool = item->pool.container;
+        pool->free_item(item);
+    }
+}
+
+
+static Entity* pool_pop_available(EntityPool* pool) {
+    Entity* item = pool->available;
+    pool->available = item->pool.next;
+    return item;
+}
+
+
+static void pool_set_available(EntityPool* pool, Entity* item) {
+    Entity* available = item;
+    item->pool.next = pool->available;
+    pool->available = available;
+}
+
 
 EntityPool* entitypool_create() {
     EntityPool* pool = malloc(sizeof(*pool));
     pool_init(pool, MAX_ENTITIES);
-    pool->free = entitypool_free;
+    pool->free_item = entity_free_pool;
     return pool;
 }
 
@@ -15,7 +55,7 @@ EntityPool* entitypool_create() {
 void* entitypool_add(EntityPool* pool) {
     Entity* item = pool_pop_available(pool);
     if(item == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "EntityPool stack overflow.\n", name);
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "EntityPool stack overflow.\n");
         return NULL;
     }
 
@@ -27,9 +67,9 @@ void* entitypool_add(EntityPool* pool) {
 }
 
 
-void entitypool_free(EntityPool* pool, Entity* item) {
+void entity_free_pool(Entity* item) {
     entity_destroy(item);
-    pool_set_available(pool, item);
+    pool_set_available(item->pool.container, item);
 }
 
 
@@ -37,9 +77,30 @@ void entity_destroy(Entity* entity) {
     lua_close(entity->L);
     int i;
     for(i = 0 ; i < entity->components.count ; ++i) {
-        void* component = entity->components.items[i];
-        void* pool = component->pool;
-        pool->free_item(pool, component);
+        ComponentItem* item = entity->components.items[i];
+        switch(item->type) {
+            case CAMERA:
+                // TODO: implement CameraComponent
+                break;
+            case PHYSIC:
+                break;
+            case TRANSFORM: {
+                TransformComponent* component = item->id;
+                transformcomponent_free_pool(component);
+                break;
+            }
+            case SPRITE:
+                break;
+            case MESH:
+                break;
+            case SCRIPT: {
+                ScriptComponent* component = item->id;
+                scriptcomponent_free_pool(component);
+                break;
+             }
+            default:
+                break;
+        }
     }
     entity->components.count = 0;
 }
--- src/entity.h
+++ src/entity.h
@@ -10,30 +10,50 @@
 #include "config.h"
 #include "pool.h"
 
+
+typedef enum {
+    CAMERA      = (1 << 0),
+    PHYSIC      = (1 << 1),
+    TRANSFORM   = (1 << 2),
+    SPRITE      = (1 << 3),
+    MESH        = (1 << 4),
+    SCRIPT      = (1 << 5)
+} ComponentType;
+
+
 typedef struct {
-    Entity* next;
-    EntityPool* pool;
+    void* id;
+    ComponentType type;
+} ComponentItem;
+
+
+typedef struct {
+    PoolItem pool;
 
     lua_State* L;
 
     struct {
-        void* items[MAX_COMPONENTS];
+        ComponentItem* items[MAX_COMPONENTS];
         unsigned int count;
     } components;
 } Entity;
 
 
-typedef struct {
+typedef struct EntityPool EntityPool;
+
+
+struct EntityPool {
     Entity items[MAX_ENTITIES];
     Entity* available;
     unsigned int count;
-    void (*free_item)(void* item);
-} EntityPool;
+    void (*free_item)(Entity* item);
+};
 
 
 EntityPool* entitypool_create();
 void* entitypool_add(EntityPool* pool);
-void entitypool_free(EntityPool* pool, Entity* item);
+void entitypool_destroy(EntityPool* pool);
+void entity_free_pool(Entity* item);
 void entity_destroy(Entity* entity);
 
 

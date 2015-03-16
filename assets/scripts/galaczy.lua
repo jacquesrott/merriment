d7577b08ebe068eddbbcf3311b6ccbfb48cdd2bd
Jacques Rott <jacques.rott@gmail.com>
2015-03-16 08:27:52 +0100

    Add message pack (de)serialization for lua instances

--- assets/scripts/galaczy.lua
+++ assets/scripts/galaczy.lua
@@ -1,5 +1,16 @@
 local ffi = require 'ffi'
+local cmsgpack = require "cmsgpack"
 
 gz = setmetatable({}, {__index = ffi.C})
 
 gz.string = ffi.string
+
+
+function serialize(instance)
+    return cmsgpack.pack(instance)
+end
+
+
+function deserialize(instance, compiled)
+    instance = cmsgpack.unpack(compiled)
+end

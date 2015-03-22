a83f2b44f75aeead05d2e1df1ea8ec6a03ec242f
Jacques Rott <jacques.rott@gmail.com>
2015-03-22 16:53:37 +0100

    Remove print in player update and move attribute assignment into init

--- assets/scripts/player.lua
+++ assets/scripts/player.lua
@@ -8,22 +8,17 @@ Player.__index = Player
 function Player.create()
     local player = {}
     setmetatable(player, Player)
-
-    player.name = "Jaqk"
-    player.life = 0
-
     return player
 end
 
 
 function Player:init()
-    self.life = 100
+    player.name = "Jaqk"
+    player.life = 100
 end
 
 
 function Player:update()
-    -- print(self.name)
-    -- print(self.life)
 end
 
 

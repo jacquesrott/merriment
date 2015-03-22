ee308fec3171414d14094a600d013e1f6b26e828
Jacques Rott <jacques.rott@gmail.com>
2015-03-22 16:46:35 +0100

    Add file path argument and handle script instance

--- editor/compiler.py
+++ editor/compiler.py
@@ -2,6 +2,7 @@ import msgpack
 import json
 import os
 import time
+import sys
 
 
 SCENES_PATH = "assets/scenes/"
@@ -17,13 +18,21 @@ COMPONENT_TYPES = {
 }
 
 def init(path):
-    return [file for file in os.listdir(path)
+    return [os.path.join(path, file) for file in os.listdir(path)
             if os.path.isfile(os.path.join(path, file))]
 
 def finish(start, end, count, action=""):
     delta = end - start
     velocity = delta / count
     print "%s files %scompiled in %.4f s - %.4f s/file" % (count, action, delta, velocity)
+
+
+def instance_pack(scene):
+    for entity in scene["entities"]:
+        for component in entity["components"]:
+            if component[0] == "script":
+                script = component[1]
+                script["instance"] = msgpack.packb(script["instance"])
 
 
 def map_types(entities, types):
@@ -33,22 +42,24 @@ def map_types(entities, types):
 
 
 if __name__ == "__main__":
-    scenes = init(EDITOR_PATH)
+    scenes = sys.argv[1:]
+
+    if not scenes:
+        scenes = init(EDITOR_PATH)
     start = time.time()
 
     for path in scenes:
-        full_path = os.path.join(EDITOR_PATH, path)
-
-        print "start    %s" % full_path
-        with open(full_path, "r+") as decompiled:
+        print "start    %s" % path
+        with open(path, "r+") as decompiled:
             scene = json.load(decompiled)
 
-        print "pack     %s" % full_path
+        print "pack     %s" % path
+        instance_pack(scene)
         map_types(scene['entities'], COMPONENT_TYPES)
         mp_scene = msgpack.packb(scene)
 
         destination = os.path.join(SCENES_PATH, scene["path"])
-        print "writ     %s" % destination
+        print "write    %s" % destination
         with open(destination, "w+b") as compiled:
             compiled.write(mp_scene)
 
--- editor/decompiler.py
+++ editor/decompiler.py
@@ -2,29 +2,45 @@ import msgpack
 import json
 import os
 import time
+import sys
 
 from editor.compiler import init, finish, map_types, EDITOR_PATH, SCENES_PATH, COMPONENT_TYPES
 
 COMPONENT_TYPES = {v: k for k, v in COMPONENT_TYPES.items()}
 
+def instance_unpack(scene):
+    for entity in scene["entities"]:
+        for component in entity["components"]:
+            if component[0] == "script":
+                script = component[1]
+                script["instance"] = msgpack.unpackb(script["instance"])
+
 
 if __name__ == "__main__":
-    scenes = init(SCENES_PATH)
+    scenes = sys.argv[1:]
+
+    if not scenes:
+        scenes = init(SCENES_PATH)
     start = time.time()
 
     for path in scenes:
-        full_path = os.path.join(SCENES_PATH, path)
-        print "start    %s" % full_path
+        print "start    %s" % path
 
-        with open(full_path, "r+b") as compiled:
+        with open(path, "r+b") as compiled:
             mp_scene = compiled.read()
 
-        print "unpack   %s" % full_path
+        print "unpack   %s" % path
         scene = msgpack.unpackb(mp_scene)
 
         map_types(scene['entities'], COMPONENT_TYPES)
 
-        full_source = os.path.join(EDITOR_PATH, scene["source"])
+        if "source" in scene:
+            full_source = os.path.join(EDITOR_PATH, scene["source"])
+        else:
+            full_source = os.path.splitext(path)[0]
+            full_source += ".json"
+            scene["source"] = os.path.basename(full_source)
+        instance_unpack(scene)
 
         print "write    %s" % full_source
         with open(full_source, "w") as decompiled:

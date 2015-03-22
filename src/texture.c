f4eb49684d3927d061a5d27d1529f1f04a290d99
Jacques Rott <jacques.rott@gmail.com>
2015-03-22 19:09:57 +0100

    Fix OpenGL error in texture loading

--- src/texture.c
+++ src/texture.c
@@ -54,7 +54,7 @@ GLuint texture_load(const char* path) {
     check_gl_errors("texture load 2D data");
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     check_gl_errors("texture set parameters");
